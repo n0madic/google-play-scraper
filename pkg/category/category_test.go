951e9c58c310880725dbb01f5db6dbce4c7c9922
Alexander Sheiko <alexander.sheiko@mgid.com>
2020-06-30 12:55:18 +0300

    Fix tests

--- pkg/category/category_test.go
+++ pkg/category/category_test.go
@@ -12,7 +12,7 @@ func TestCategory(t *testing.T) {
 	sortList := []store.Sort{store.SortHelpfulness, store.SortNewest, store.SortRating}
 	for _, sort := range sortList {
 		l, err := New(store.Game, sort, store.AgeFiveUnder, Options{
-			Country:  "ru",
+			Country:  "us",
 			Language: "us",
 			Number:   resultsCount,
 		})
