79181b36d6496c00faf85567989dcb6d3bde2038
Alexander Sheiko <alexander.sheiko@mgid.com>
2022-02-26 20:37:28 +0200

    Fix suggest

--- pkg/suggest/suggest.go
+++ pkg/suggest/suggest.go
@@ -1,49 +1,24 @@
 package suggest
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/n0madic/google-play-scraper/internal/util"
 	"github.com/n0madic/google-play-scraper/pkg/app"
 )
-
-const suggURL = "https://market.android.com/suggest/SuggRequest"
 
 // Options type alias
 type Options = app.Options
 
 // Get returns up to five suggestion to complete a search query term
 func Get(term string, options Options) (list []string, err error) {
-	req, err := http.NewRequest("GET", suggURL, nil)
+	payload := "f.req=%5B%5B%5B%22IJ4APc%22%2C%22%5B%5Bnull%2C%5B%5C%22" + term + "%20e%5C%22%5D%2C%5B10%5D%2C%5B2%5D%2C4%5D%5D%22%2Cnull%2C%22generic%22%5D%5D%5D"
+
+	js, err := util.BatchExecute(options.Country, options.Language, payload)
 	if err != nil {
 		return nil, err
 	}
 
-	q := req.URL.Query()
-	q.Set("json", "1")
-	q.Set("query", term)
-	q.Add("gl", options.Country)
-	q.Add("hl", options.Language)
-	req.URL.RawQuery = q.Encode()
-
-	body, err := util.DoRequest(req)
-	if err != nil {
-		return nil, err
-	}
-
-	var jslist []struct {
-		S string `json:"s"`
-	}
-	err = json.Unmarshal(body, &jslist)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, sugg := range jslist {
-		if sugg.S != "" {
-			list = append(list, sugg.S)
-		}
+	for _, sugg := range util.GetJSONArray(js, "0.0.#.0") {
+		list = append(list, sugg.String())
 	}
 
 	return list, nil
